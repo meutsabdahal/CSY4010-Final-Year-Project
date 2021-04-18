66bd9e67f1092a09497f1b6acb26d3f46335f033
Utsab Dahal <70103872+meutsabdahal@users.noreply.github.com>
2021-04-18 22:24:52 +0545

    Update vendorTemplate.php

--- templates/vendorTemplate.php
+++ templates/vendorTemplate.php
@@ -4,38 +4,40 @@
 ?>
 
 <section>
-
-    <div class="product-details">
-        <div class="ml-5 mt-4 w-50 text-center float-left product-description">
-            <h4 class="font-italic">Vendor Name</h4>
-
-            <!-- displaying rating of the vendor -->
-            <img src="{{ asset('svg/rating.svg') }}" class="ml-2">
-            <img src="{{ asset('svg/rating.svg') }}" class="ml-2">
-            <img src="{{ asset('svg/rating.svg') }}" class="ml-2">
-            <img src="{{ asset('svg/rating.svg') }}" class="ml-2">
-            <img src="{{ asset('svg/rating.svg') }}" class="ml-2">
-            <br>
-
-            <br>
-            Lorem Ipsum is simply dummy text of the printing
-            and typesetting industry.Lorem Ipsum has been the
-            industry's  standard dummy text ever since the 
-            1500s, when anunknown printer took a
-            galley of type and scrambled it to make a type specimen 
-            book. It has survived not  only five centuries,
-            but also the leap into electronic typesetting, remaining 
-            essentially unchanged. 
-            It was popularised in the
-            1960s with the release of Letraset sheets 
-            containing Lorem Ipsum passages, and more recently with
-            desktop
-            publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
+  
+    <div class="ml-5 mt-3 w-50  product-description">
+        <h4 class="font-italic">Vendor Name</h4>
+        <br>
+        Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry.Lorem Ipsum has been the
+        industry's  standard dummy text ever since the 
+        1500s, when anunknown printer took a
+        galley of type and scrambled it to make a type specimen 
+        book. It has survived not  only five centuries,
+        but also the leap into electronic typesetting, remaining 
+        essentially unchanged. 
+        It was popularised in the
+        1960s with the release of Letraset sheets 
+        containing Lorem Ipsum passages, and more recently with
+        desktop
+        publishing software like
+        Aldus PageMaker including versions of Lorem Ipsum.
             
+    </div>
+    
+    <h3 class="ml-5 mt-4 font-italic">Reviews of Vendor Name's Products</h3>
+    <div class="review-container shadow bg-white mt-3">
+        Customer Name
+        <div class="review-box mt-2">
+            <center> Review of the Product</center>
         </div>
-        <div class=" mt-4 ml-5">
-            <img src="{{ asset('img/shoe.jpg') }}" class="ml-5 mt-4" >
+    </div>
+
+  
+    <div class="review-container shadow bg-white mt-3"> 
+        Customer Name
+        <div class="review-box mt-2">
+            <center> Review of the Product</center>
         </div>
     </div>
 </section>
